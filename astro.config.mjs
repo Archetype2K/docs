46c3436b1e40357be518b65d25360d6dbba79d9d
Pasindu Dinal Sumanasekara <51123579+mrmihi@users.noreply.github.com>
2023-01-27 00:50:59 +0530

    deployent

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mrmihi.dev',
-  base: '/notes',
+  site: 'https://Archtype2K.github.io',
+  base: '/docs',
   integrations: [tailwind()]
 });
